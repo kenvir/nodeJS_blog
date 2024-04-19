fe5e49092cfd347ee54c5fd197a02432f01dec82
kenvir <viet02092001@gmail.com>
2024-04-19 14:54:06 +0700

    Layout & Static file

---
+++ blog/src/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const morgan = require("morgan");
+// const { engine } = require("express-handlebars");
+const handlebars = require("express-handlebars").engine;
+const path = require("path");
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// HTTP logger
+app.use(morgan("combined"));
+
+// Template engine
+// app.engine("handlebars", engine());
+app.engine(
+  "hbs",
+  handlebars({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/news", (req, res) => {
+  res.render("news");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
